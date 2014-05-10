e60716e196298a3d0027cff7a98436669fae98af
Drew Perttula <drewp@bigasterisk.com>
2014-05-09 23:51:24 -0700

    digispark program optimizations

--- bigeyestrip/bigeyestrip.cpp
+++ bigeyestrip/bigeyestrip.cpp
@@ -11,41 +11,47 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream
 //   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
 //   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(3, 5, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(26, 5, NEO_GRB + NEO_KHZ800);
 
-const int debugLed = 1;
+// max about 26 with malloc; 36 works with a static array in the class
 
-int main(void) {
-  init();
-  pinMode(debugLed, OUTPUT);
-  strip.begin();
-  
+#define debugLed 1
+void intro() {
   uint32_t red = strip.Color(255,0,0), black = strip.Color(0,0,0);
   strip.setPixelColor(0,   red); strip.show(); delay(100);
   strip.setPixelColor(0, black); strip.show(); delay(100);
   strip.setPixelColor(0,   red); strip.show(); delay(100);
   strip.setPixelColor(0, black); strip.show(); delay(100);
+}
+int main(void) {
+  init();
+  pinMode(debugLed, OUTPUT);
+  strip.begin();
+  intro(); 
   DigiUSB.begin();
   DigiUSB.refresh();
  
-  uint8_t head, i, r, g, b;
+  uint8_t i;
+  int r, g, b;
   while (1) {
     digitalWrite(debugLed, 0);
     while (DigiUSB.available() <= 0){
       DigiUSB.refresh();
-      delay(1);
     }
 
-    head = DigiUSB.read();
+    i = DigiUSB.read();
     digitalWrite(debugLed, 1);
 
-    if (head != 0x60) {
+    if (i != 0x60) {
       continue;
     }
     for (i=0; i < strip.numPixels(); i++) {
-      while (!DigiUSB.available()) DigiUSB.refresh(); r = DigiUSB.read();  
-      while (!DigiUSB.available()) DigiUSB.refresh(); g = DigiUSB.read();  
-      while (!DigiUSB.available()) DigiUSB.refresh(); b = DigiUSB.read();  
+      while (DigiUSB.available() < 3) {
+        DigiUSB.refresh();
+      }
+      r = DigiUSB.read();
+      g = DigiUSB.read();
+      b = DigiUSB.read();
       strip.setPixelColor(i, strip.Color(r, g, b));
     }
     strip.show(); 
